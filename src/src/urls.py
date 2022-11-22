7aaef101351a553af770fc81e0d6befba5e13f65
b1sar <cebrailylmz02@gmail.com>
2022-11-23 00:11:58 +0300

    Integrate Github OAuth2

--- src/src/urls.py
+++ src/src/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('gitboard.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
